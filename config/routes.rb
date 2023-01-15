a28085142fc842125f802a7b16ebb659a93f6afc
Gabriel Ardevol <artsdevol@gmail.com>
2023-01-15 20:09:33 +0100

    first push

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # root                   to: "pages#home"
+  root to: "pages#landing"
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
